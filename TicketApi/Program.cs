56654e1d6f046be253e9eb7f10474d1f1ad06805
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2025-04-10 16:39:15 -0600

    fix: cors

--- TicketApi/Program.cs
+++ TicketApi/Program.cs
@@ -9,13 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
 using (var scope = app.Services.CreateScope())
 {
